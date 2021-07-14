cc9f7f8cd0bb3c6a7a099083e31670394c7ebef6
aashishmeenaets <aashish.meena@expresstechsoftwares.com>
2021-07-14 20:02:47 +0530

    fixed : #5

--- admin/partials/pages/memberpress-discord-documentation.php
+++ admin/partials/pages/memberpress-discord-documentation.php
@@ -8,12 +8,12 @@
 		<li>
 			 <label><?php echo __( 'Step-by-Step Written guide on how to connect the plugin with Discord server' ); ?>
 			</label>
-			<a href="https://www.expresstechsoftwares.com/step-by-step-documentation-guide-on-how-to-connect-pmpro-and-discord-server-using-discord-addon/" target="_blank" ><?php echo __( 'Click Here' ); ?> <i class="fa fa-mouse-pointer" aria-hidden="true"></i></a>
+			<a href=" https://www.expresstechsoftwares.com/step-by-step-documentation-guide-on-how-to-connect-memberpress-and-discord-server-using-discord-addon" target="_blank" ><?php echo __( 'Click Here' ); ?> <i class="fa fa-mouse-pointer" aria-hidden="true"></i></a>
 		</li>
 		<li>
 			<label><?php echo __( 'Features and other documentation' ); ?>
 			</label>
-			<a href="https://www.expresstechsoftwares.com/pmpro-official-discord-add-on" target="_blank" ><?php echo __( 'Click Here' ); ?> <i class="fa fa-mouse-pointer" aria-hidden="true"></i></a>  
+			<a href="https://www.expresstechsoftwares.com/memberpress-discord-add-on/" target="_blank" ><?php echo __( 'Click Here' ); ?> <i class="fa fa-mouse-pointer" aria-hidden="true"></i></a>  
 		</li>
 	</ul>
 </div>
