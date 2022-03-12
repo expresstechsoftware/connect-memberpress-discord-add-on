9364a6d1f5eb82c215cf575196009604cea89a97
Ravi Soni <ravisoni6262@gmail.com>
2022-03-12 17:07:23 +0530

    Update plugin name in main file

--- memberpress-discord.php
+++ memberpress-discord.php
@@ -9,7 +9,7 @@
  * @package           Memberpress_Discord
  *
  * @wordpress-plugin
- * Plugin Name:       MemberPress Discord
+ * Plugin Name:       MemberPress Discord Add On
  * Plugin URI:        https://www.expresstechsoftwares.com/memberpress-discord-add-on/
  * Description:       Allow memberpress users to connect their site to discord and allow them to be part of your discord community, site admin can allow discord roles based on the membership a member have and can sell private content in role specific discord channels.
  * Version:           1.0.0
