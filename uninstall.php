0aba5d9d466f5b55cf8da430c4142923c5da9386
Younes DRO <dro66_8@hotmail.fr>
2023-05-25 22:26:03 +0100

    Fix Plugin slug on uninstall request

--- uninstall.php
+++ uninstall.php
@@ -1,6 +1,7 @@
 <?php
 /**
  * Fired when the plugin is uninstalled.
+ *
  * @link       https://www.expresstechsoftwares.com
  * @since      1.0.0
  *
@@ -9,15 +10,14 @@
 
 // If uninstall not called from WordPress, then exit.
 if ( defined( 'WP_UNINSTALL_PLUGIN' )
-		&& $_REQUEST['plugin'] == 'connect-memberpress-discord-add-on/memberpress-discord.php'
-		&& $_REQUEST['slug'] == 'connect-memberpress-to-discord'
+		&& $_REQUEST['plugin'] == 'expresstechsoftwares-memberpress-discord-add-on/memberpress-discord.php'
+		&& $_REQUEST['slug'] == 'expresstechsoftwares-memberpress-discord-add-on'
 	&& wp_verify_nonce( $_REQUEST['_ajax_nonce'], 'updates' )
   ) {
-	$ets_memberpress_discord_data_erases            = sanitize_text_field( trim( get_option( 'ets_memberpress_discord_data_erases' ) ) );
-	if ( $ets_memberpress_discord_data_erases == true ){
+	$ets_memberpress_discord_data_erases = sanitize_text_field( trim( get_option( 'ets_memberpress_discord_data_erases' ) ) );
+	if ( $ets_memberpress_discord_data_erases == true ) {
 		global $wpdb;
 		$wpdb->query( 'DELETE FROM ' . $wpdb->prefix . "usermeta WHERE `meta_key` LIKE '_ets_memberpress_discord%'" );
-		$wpdb->query( 'DELETE FROM ' . $wpdb->prefix . "options WHERE `option_name` LIKE 'ets_memberpress_discord_%'" );		
+		$wpdb->query( 'DELETE FROM ' . $wpdb->prefix . "options WHERE `option_name` LIKE 'ets_memberpress_discord_%'" );
 	}
-
 }
