552a19c60c4aef18fc90a9a29e3ca98601700f6e
Younes DRO <dro66_8@hotmail.fr>
2023-05-25 21:14:04 +0100

    Fix empty space value

--- admin/partials/pages/memberpress-discord-advance.php
+++ admin/partials/pages/memberpress-discord-advance.php
@@ -195,40 +195,22 @@ $current_url                                        = ets_memberpress_discord_ge
 	<tr>
 		<th scope="row"><?php esc_html_e( 'How many times a failed API call should get re-try', 'expresstechsoftwares-memberpress-discord-add-on' ); ?></th>
 		<td> <fieldset>
-		<input name="ets_memberpress_discord_retry_api_count" type="number" min="1" id="ets_memberpress_discord_retry_api_count" value="
-		<?php
-		if ( isset( $retry_api_count ) ) {
-			echo esc_attr( $retry_api_count );
-		} else {
-			echo 1; }
-		?>
-		">
+		<?php $retry_api_count_value = ( isset( $retry_api_count ) ) ? $retry_api_count : 1; ?>	
+		<input name="ets_memberpress_discord_retry_api_count" type="number" min="1" id="ets_memberpress_discord_retry_api_count" value="<?php echo esc_attr( $retry_api_count_value ); ?>">
 		</fieldset></td>
 	  </tr> 
 	  <tr>
 		<th scope="row"><?php esc_html_e( 'Set job queue concurrency', 'expresstechsoftwares-memberpress-discord-add-on' ); ?></th>
 		<td> <fieldset>
-		<input name="set_job_cnrc" type="number" min="1" id="set_job_cnrc" value="
-		<?php
-		if ( isset( $set_job_cnrc ) ) {
-			echo esc_attr( $set_job_cnrc );
-		} else {
-			echo 1; }
-		?>
-		">
+		<?php $set_job_cnrc_value = ( isset( $set_job_cnrc ) ) ? $set_job_cnrc : 1; ?>	
+		<input name="set_job_cnrc" type="number" min="1" id="set_job_cnrc" value="<?php echo esc_attr( $set_job_cnrc_value ); ?>">
 		</fieldset></td>
 	  </tr>
 	  <tr>
 		<th scope="row"><?php esc_html_e( 'Set job queue batch size', 'expresstechsoftwares-memberpress-discord-add-on' ); ?></th>
 		<td> <fieldset>
-		<input name="set_job_q_batch_size" type="number" min="1" id="set_job_q_batch_size" value="
-		<?php
-		if ( isset( $set_job_q_batch_size ) ) {
-			echo esc_attr( $set_job_q_batch_size );
-		} else {
-			echo 10; }
-		?>
-		">
+		<?php $set_job_q_batch_size_value = ( isset( $set_job_q_batch_size ) ) ? $set_job_q_batch_size : 10; ?>
+		<input name="set_job_q_batch_size" type="number" min="1" id="set_job_q_batch_size" value="<?php echo esc_attr( $set_job_q_batch_size_value ); ?>">
 		</fieldset></td>
 	  </tr>
 	<tr>
