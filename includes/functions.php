b5643f010d4fcb14e645f21850df9b6f360b3f92
Younes DRO <dro66_8@hotmail.fr>
2023-12-15 09:33:31 +0100

    remove the is_duplicate_hash_key() function . since it s a log we can keep duplicate content

--- includes/functions.php
+++ includes/functions.php
@@ -134,10 +134,10 @@ function write_api_response_logs_v2( $response_arr, $user_id, $backtrace_arr = a
 	$api_logger = new ETS_Memberpress_Discord_Api_Logger();
 
     if ( is_array( $response_arr ) && array_key_exists( 'code', $response_arr ) ) {
-        $hash_key = md5( json_encode( $response_arr ) . time() );
-        error_log ( '1 ' . $hash_key, true );
+        $hash_key = md5( json_encode( $response_arr ) );
+        
 
-        if ( ! is_duplicate_hash_key( $hash_key ) ) {
+        
             $api_logger->log_api_request(
                 array(
                     'api_endpoint'           => '',
@@ -153,12 +153,12 @@ function write_api_response_logs_v2( $response_arr, $user_id, $backtrace_arr = a
                     'hash_key'               => $hash_key,
                 )
             );
-        }
+        
     } elseif ( is_array( $response_arr ) && array_key_exists( 'error', $response_arr ) ) {
-        $hash_key = md5( json_encode( $response_arr ) . time() );
-        error_log('2 ' . $hash_key, true);
+        $hash_key = md5( json_encode( $response_arr ) );
+        
 
-        if ( ! is_duplicate_hash_key( $hash_key ) ) {
+        
             $api_logger->log_api_request(
                 array(
                     'api_endpoint'           => '',
@@ -174,7 +174,7 @@ function write_api_response_logs_v2( $response_arr, $user_id, $backtrace_arr = a
                     'hash_key'               => $hash_key, 
                 )
             );
-        }
+        
     } elseif ( get_option( 'ets_memberpress_discord_log_api_response' ) == true ) {
         if ( is_array( $response_arr ) ) {
             $api_endpoint = $response_arr['api_endpoint'];
@@ -186,11 +186,8 @@ function write_api_response_logs_v2( $response_arr, $user_id, $backtrace_arr = a
             $error_detail_code = $response_arr['error_detail_code'];
             $discord_user_id = $response_arr['discord_user_id'];
 
-            $hash_key = md5( json_encode( $response_arr ) . time() );
+            $hash_key = md5( json_encode( $response_arr ) );
 
-            if ( ! is_duplicate_hash_key( $hash_key ) ) {
-                // update_option();
-                error_log( ' Step 3 :' . $hash_key);
 
                 $api_logger->log_api_request(
                     array(
@@ -207,25 +204,11 @@ function write_api_response_logs_v2( $response_arr, $user_id, $backtrace_arr = a
                         'hash_key'               => 'a9bf6c5d8b25f8c6195cc6ef8981c847',
                     )
                 );
-            }
+            
         }
     }
 }
 
-/**
- * Check if the hash key already exists in the database
- *
- * @param string $hash_key
- * @return bool
- */
-function is_duplicate_hash_key( $hash_key ) {
-	global $wpdb;
-	$table_name      = $wpdb->prefix . 'ets_memberpress_discord_api_logs';
-	$query = $wpdb->prepare( "SELECT COUNT(*) FROM $table_name WHERE hash_key = %s", $hash_key );
-	$result = $wpdb->get_var( $query );
-	
-	return ( $result > 0 );
-}
 
 
 
